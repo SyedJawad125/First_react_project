d34b05bcf6e4f6e11d0942815305dc4ae88a9375
Syed Haider Ali <haiderali@joyngroup.us>
2024-05-08 15:43:44 +0500

    2nd Changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import Chai from "./Chai"
 function App() {
 
   return (
-    <h1>React Learning in ViteReact</h1>
+    // <h1>React Learning in ViteReact</h1>
+    <div>
+      <Chai/>
+      <h1>dfhind </h1>
+    </div>
   )
 }
 
